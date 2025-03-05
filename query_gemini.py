30061469fe3af9493d8ccf77359dc5b18f08c7d0
Alex Richert <alexander.richert@noaa.gov>
2025-03-04 16:49:18 -0800

    add mistral support

--- query_gemini.py
+++ query_gemini.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("query_filename", type=str, help="Path to file containing query text")
 parser.add_argument("output_filename", type=str, help="Path to output file")
+parser.add_argument("--backend", choices=("gemini-2.0", "mistral"), default="gemini-2.0", help="LLM to use")
 apikey_group = parser.add_mutually_exclusive_group(required=True)
 apikey_group.add_argument("-k", "--key-variable", help="Name of env variable containing API key")
 apikey_group.add_argument("-f", "--key-file", help="Path to file containing API key")
@@ -28,14 +29,23 @@ else:
   with open(args.key_file, "r") as key_file:
     api_key = key_file.read().strip()
 
-url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={api_key}"
-params = {'key': api_key,}
-headers = {'Content-Type': 'application/json',}
-json_send = {'contents': [{'parts': [{'text': query_text,},],},],}
-
-response = requests.post(url, params=params, headers=headers, json=json_send)
-
-review = json.loads(response.content.decode())["candidates"][0]["content"]["parts"][0]["text"]
+if args.backend=="gemini-2.0":
+  url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+  params = {'key': api_key,}
+  headers = {'Content-Type': 'application/json',}
+  json_send = {'contents': [{'parts': [{'text': query_text,},],},],}
+  response = requests.post(url, params=params, headers=headers, json=json_send)
+  review = json.loads(response.content.decode())["candidates"][0]["content"]["parts"][0]["text"]
+elif args.backend=="mistral":
+  url = "https://api.mistral.ai/v1/chat/completions"
+  headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': f'Bearer {api_key}',
+  }
+  json_send = {"model": "mistral-large-latest", "messages": [{"role": "user", "content": query_text}]}
+  response = requests.post(url, headers=headers, json=json_send)
+  review = json.loads(response.content.decode())["choices"][0]["message"]["content"]
 
 with open(args.output_filename, "w") as output_file:
   output_file.write(review)

9f24166d4394ccf25c5ee5f07a8d384551f95f5c
Alex Richert <alexander.richert@noaa.gov>
2025-03-18 11:10:00 -0700

    add support for GitHub Models

--- query_llm_api.py
+++ query_llm_api.py
@@ -4,11 +4,11 @@ import argparse
 
 parser = argparse.ArgumentParser(
                     prog='query_gemini.py',
-                    description='Query Gemini API')
+                    description='Query various LLM APIs')
 
 parser.add_argument("query_filename", type=str, help="Path to file containing query text")
 parser.add_argument("output_filename", type=str, help="Path to output file")
-parser.add_argument("--backend", choices=("gemini-2.0", "mistral"), default="gemini-2.0", help="LLM to use")
+parser.add_argument("--backend", choices=("gemini-2.0", "mistral", "gh-gpt-4o", "gh-phi-4"), default="gemini-2.0", help="LLM to use")
 apikey_group = parser.add_mutually_exclusive_group(required=True)
 apikey_group.add_argument("-k", "--key-variable", help="Name of env variable containing API key")
 apikey_group.add_argument("-f", "--key-file", help="Path to file containing API key")
@@ -17,6 +17,7 @@ args = parser.parse_args()
 
 import json
 import os
+import re
 import requests
 
 if args.query_filename:
@@ -37,6 +38,7 @@ if args.backend=="gemini-2.0":
   headers = {'Content-Type': 'application/json',}
   json_send = {'contents': [{'parts': [{'text': query_text,},],},],}
   response = requests.post(url, params=params, headers=headers, json=json_send)
+  assert response.status_code==200, f"ERROR: Request failed (status code not 200; was {response.status_code})"
   review = json.loads(response.content.decode())["candidates"][0]["content"]["parts"][0]["text"]
 elif args.backend=="mistral":
   url = "https://api.mistral.ai/v1/chat/completions"
@@ -47,7 +49,25 @@ elif args.backend=="mistral":
   }
   json_send = {"model": "mistral-large-latest", "messages": [{"role": "user", "content": query_text}]}
   response = requests.post(url, headers=headers, json=json_send)
-  assert response.status_code==200, "ERROR: Request failed (status code not 200)"
+  assert response.status_code==200, f"ERROR: Request failed (status code not 200; was {response.status_code})"
+  review = json.loads(response.content.decode())["choices"][0]["message"]["content"]
+elif args.backend.startswith("gh"):
+  url = "https://models.inference.ai.azure.com/chat/completions"
+  headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {api_key}',
+  }
+  json_send = {
+    "messages": [
+      {
+        "role": "user",
+        "content": query_text,
+      }
+    ],
+    "model": re.sub("^gh-", "", args.backend),
+  }
+  response = requests.post(url, headers=headers, json=json_send)
+  assert response.status_code==200, f"ERROR: Request failed (status code not 200; was {response.status_code})"
   review = json.loads(response.content.decode())["choices"][0]["message"]["content"]
 
 with open(args.output_filename, "w") as output_file:
